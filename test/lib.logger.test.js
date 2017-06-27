81b586133be47258e0613f373dff78f5cd8175d3
osher <osher.filter@gmail.com>
2017-06-27 17:51:04 +0300

    refactor logger test

--- test/lib.logger.test.js
+++ test/lib.logger.test.js
@@ -1,12 +1,55 @@
 const sut = require('../lib/logger');
 
 module.exports = {
-    'the logger' : {
-      'should init with settings' : () => {
-           sut.configure()
+  'lib/logger' : {
+    'should be a module object' : () => {
+        Should(sut).be.an.Object()
+    },
+    '.configure(logCfg)': {
+      'should be a function that expects 1 argument- loggerConfig' : () => {
+          Should(sut.configure).be.a.Function().have.property('length', 1)
       },
-      'should init with defaults': () => {
-           sut.configure( { levels: { "[all]" : "FATAL" } } )
+      'when initiated without providing any configs' : {
+        'should not fail' : () => {
+            sut.configure()
+        },
+        'should set the logLevel to INFO' : () => {
+            Should(sut.of('foo').level.levelStr).eql("INFO")
+        }
+      },
+      'when initiated with explicit settings': {
+        'should not fail' : () => {
+            sut.configure( { levels: { "[all]" : "FATAL" } } )
+        },
+        'should set the logLevel to the level noted in the settings' : () => {
+            Should(sut.of('foo').level.levelStr).eql("FATAL")
+        }
+      }
+    },
+    '.of(name)' : {
+      'should be a function that expects 1 argument - name' : () => {
+          Should(sut.configure).be.a.Function().have.property('length', 1)
+      },
+      'when used with a name' : { 
+        'should return a logger instance with methods:' : block( () => {
+          const log = sut.of('some-name');
+          const suite = {}
+          
+          "Fatal|Warn|Info|Debug|Trace".split("|").forEach(m => {
+              suite[m.toLowerCase()] = () => {
+                log.should.have.property(m.toLowerCase()).be.a.Function()
+              }
+              
+              suite["is" + m + "Enabled"] = () => {
+                log.should.have.property("is" + m + "Enabled").be.a.Function()
+              }
+
+          })        
+
+          return suite
+        
+        })
       }
     }
+  }
 }
